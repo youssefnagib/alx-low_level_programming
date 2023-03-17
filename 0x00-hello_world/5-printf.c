aaec1eea5050bc7ca0ea1c79b07809b8f43ff1cf
youssefnagib <youssefnagib@yahoo.com>
2023-03-16 18:39:03 -0700

    yalla

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: as program that prints with code function
+ *
+ * Return: Alawys 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
