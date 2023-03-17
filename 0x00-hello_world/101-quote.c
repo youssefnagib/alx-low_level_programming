1310945f1261b2c8065c18ad6bd94cc0b0a7a6cf
youssefnagib <youssefnagib@yahoo.com>
2023-03-16 20:03:04 -0700

    yalla

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,17 @@
-#include<unistd.h>
+#include <unistd.h>0
+
 /**
  * main - Entry point
  *
  * Description: print a qoute;
  *
- * Return: 1 (fail).
+ * Return: 1 (error).
  */
+
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(1, quo, 59);
 	return (1);
 }
