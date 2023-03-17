c1f8816dbce49f84d7e372ae456269342cf241ff
youssefnagib <youssefnagib@yahoo.com>
2023-03-16 18:32:49 -0700

    yalla

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(void){
+/**
+ * main - Entry point
+ *
+ * Description: as program that prints with code function
+ *
+ * Return: Alawys 0 (Success)
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
