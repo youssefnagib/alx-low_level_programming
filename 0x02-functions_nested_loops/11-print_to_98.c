23a8f17cb5dddacfb9875cb7233b2bf4ae7a63ca
youssefnagib <youssefnagib@yahoo.com>
2023-03-26 13:01:19 -0700

    yalla go

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_to_98 - print to 98
+*
+* @n: input number
+*/
+
+void print_to_98(int n)
+{
+	int x
+	if (n <= 98)
+	for (x = n; x <= 98 ; x++)
+		printf('&d', x);
+	else
+		for (x = n; x >= n; x--)
+			printf('%d', x);
+}
