5b6df00b741057d9529d5a855f8de0ed5209fb6d
youssefnagib <youssefnagib@yahoo.com>
2023-03-26 13:10:55 -0700

    yalla go

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,10 +9,11 @@
 void print_to_98(int n)
 {
 	int x;
-	if (n <= 98)
-	for (x = n; x <= 98 ; x++)
-		printf("%d", x);
+	if (n < 98)
+	for (x = n; x < 98 ; x++)
+		printf("%d, ", x);
 	else
-		for (x = n; x >= n; x--)
-			printf("%d", x);
+		for (x = n; x > n; x--)
+			printf("%d, ", x);
+	printf("98\n")
 }
