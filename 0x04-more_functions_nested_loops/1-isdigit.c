582bede229e838494b74f061ec73004babce1829
youssefnagib <youssefnagib@yahoo.com>
2023-03-26 17:47:11 -0700

    yalla go

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _isdigit - if digit return 1 else return 0
  *
@@ -6,7 +7,9 @@
  *
  * Return: 1 if digit , 0 if not
  */
+
 int _isdigit(int c)
+
 {
 	if (c >= 48 && c <= 58)
 		return (1);
