fcd1cfe65cd3f631094f00ff89421b78d624ccde
youssefnagib <youssefnagib@yahoo.com>
2023-03-28 13:37:39 -0700

    yalla go

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,9 +5,11 @@ void rev_string(char *s)
 	int c, m, m1;
 	char tmp;
 
-	for (m = 0; s[m] != '\0'; ++m);
+	for (m = 0; s[m] != '\0'; ++m)
+	{
 	m1 = m - 1;
-	for (c=0; c < m/2; c++)
+	}
+	for (c = 0; c < m / 2; c++)
 	{
 		tmp = s[c];
 		s[c] = s[m1];
