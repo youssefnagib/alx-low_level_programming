09e948512821ffb66c91a054608aecd09333ca04
youssefnagib <youssefnagib@yahoo.com>
2023-03-27 17:48:33 -0700

    yalla bena

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,16 @@
 #include "main.h"
-/***/
+/**
+ * swap_int - swab two values
+ *
+ * @a: input value
+ *
+ * @b: input value
+ *
+ */
 void swap_int(int *a, int *b)
 {
 	int c;
+
 	c = *a;
 	*a = *b;
 	*b = c;
