be6cfa92b4addc4db99fd9d572791dcc6deac32a
youssefnagib <youssefnagib@yahoo.com>
2023-03-28 16:54:25 -0700

    yalla go

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,11 @@
 #include "main.h"
-/***/
+/**
+ * print_array - print num
+ *
+ * @a: array
+ *
+ * @n: numper in array
+ */
 void print_array(int *a, int n)
 {
 	int i;
@@ -8,8 +14,6 @@ void print_array(int *a, int n)
 	{
 		if (i != (n - 1))
 			printf("%d, ", a[i]);
-		else
-			printf("%d", a[i]);
 	}
 	_putchar('\n');
 }
