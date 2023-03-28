674eb50a80ded18497a35749299878aa697a6730
youssefnagib <youssefnagib@yahoo.com>
2023-03-28 13:40:13 -0700

    yalla go

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,17 @@
 #include "main.h"
-/***/
+/**
+ * rev_string - function that rev
+ *
+ * @s: is the string
+ */
 void rev_string(char *s)
 {
 	int c, m, m1;
 	char tmp;
 
 	for (m = 0; s[m] != '\0'; ++m)
-	{
+		;
 	m1 = m - 1;
-	}
 	for (c = 0; c < m / 2; c++)
 	{
 		tmp = s[c];
