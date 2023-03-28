71c7dcb31832dfbfcbc807d29259f8599365f4d5
youssefnagib <youssefnagib@yahoo.com>
2023-03-28 11:25:54 -0700

    yalla go

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - function that print in rev way
+ *
+ * @s: is the parameter
+ *
+ * Return: (rev)
+ */
+void print_rev(char *s)
+{
+	while (s[c])
+	{
+		c++;
+	}
+	while (c--)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
