4659f7c2c0496565212dbdac8c2ca0007e2a4360
youssefnagib <youssefnagib@yahoo.com>
2023-03-28 14:05:22 -0700

    yalla go

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *
+ */
+void puts_half(char *str)
+{
+	int i, c;
+
+	for (i = 0; str[i] != '\0'; ++i)
+		;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
