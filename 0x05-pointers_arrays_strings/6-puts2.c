d74c5ed01eb54ef9560c575b2a7581f1e6038229
youssefnagib <youssefnagib@yahoo.com>
2023-03-28 13:57:01 -0700

    yalla go

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - function taht print even only
+ *
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
