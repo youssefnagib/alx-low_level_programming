804752333660dab2cf08a4e86de55ff7fab03033
youssefnagib <youssefnagib@yahoo.com>
2023-03-30 15:04:06 -0700

    gooo

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int e = 0;
+	int equal = 0;
 
 	if (*s1 != *s2)
 	{
-		e = ((int)*s1 - 48) - ((int)*s2 - 48);
+		equal = ((int)*s1 - 48) - ((int)*s2 - 48);
 		break;
 	}
 	s1++;
 	s2++;
-	return (e);
+	return (equal);
 }
