76302560c9b6f747cadb55d07ff0e170cf9617ff
youssefnagib <youssefnagib@yahoo.com>
2023-03-30 15:21:39 -0700

    gooo

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverse string
+ *
+ * @a: is pointer that reverse
+ *
+ * @n: is pointer that help to reverse
+ *
+ * Return: n
+ */
+void reverse_array(int *a, int n)
+{
+	while (a++)
+		;
+	for (n = 0; n != '\n'; n++)
+	{
+		n = a;
+		a--;
+	}
+	return (n);
+}
