3d9e15822bee5725df7acd3a186b01550907c59c
youssefnagib <youssefnagib@yahoo.com>
2023-03-30 12:15:11 -0700

    gooo

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,15 @@
 #include "main.h"
-/***/
+/**
+ * _strncat - function that print the num's that pointed to
+ *
+ * @dest: the pointer the description input
+ *
+ * @src: the pointer the source input
+ *
+ * @n: the pointer the num's in the word that need to print in the sec
+ *
+ * Return: dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 	int o, i;
