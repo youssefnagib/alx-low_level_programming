69913ecbdff6f5ffd5543e8e13abeb6171f51e66
youssefnagib <youssefnagib@yahoo.com>
2023-03-30 17:58:20 -0700

    yalago

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/***/
+char *cap_string(char *s)
+{
+	int i;
+
+i = 0;
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+		i++;
+	}
+	return (s);
+}
