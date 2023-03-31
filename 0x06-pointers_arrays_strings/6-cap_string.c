d8f7ec2c0811930779c0d28969a051e7c5f125b9
youssefnagib <youssefnagib@yahoo.com>
2023-03-30 19:30:24 -0700

    kaml

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,34 @@
 #include "main.h"
+
 /**
- * cap_string - function caplitze
+ * cap_string - capitalizes everey word of a string
  *
- * @s: string
+ * @s: string to modify
  *
- * Return: char
+ * Return: string
  */
 char *cap_string(char *s)
 {
-	int i;
+	int i, j;
 
-i = 0;
-	while (*(s + i))
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-			*(s + i) -= 'a' - 'A';
-		i++;
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
 	}
 	return (s);
 }
