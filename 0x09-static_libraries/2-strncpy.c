0c754edde68207501a4087c1ab8a80ee7090d445
youssefnagib <youssefnagib@yahoo.com>
2023-04-10 15:09:38 -0700

    ohh

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - copy string
+ *
+ * @dest: pointer discription input
+ *
+ * @src: pointer source input
+ *
+ * @n: bytes of src
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
