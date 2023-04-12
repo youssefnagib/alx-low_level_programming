b3feb4a17aa26bad2a9e32893c4a3faef3a71737
youssefnagib <youssefnagib@yahoo.com>
2023-04-11 19:29:24 -0700

    yalla

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_array - array
+ *
+ * @size: size of the array
+ *
+ * @c: specific char
+ *
+ * Return: a pointer to array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+{
+	return (NULL);
+}
+
+	char *arr = (char *) malloc(size * sizeof(char));
+
+	if (arr == NULL)
+{
+	return (NULL);
+}
+
+	for (unsigned int i = 0; i < size; i++)
+{
+	arr[i] = c;
+}
+
+	return (arr);
+}
