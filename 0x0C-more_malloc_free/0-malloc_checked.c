21d299400e3ae54d2f477dcde958cf6f70464841
youssefnagib <youssefnagib@yahoo.com>
2023-04-13 16:51:53 -0700

    helahela

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *malloc_checked - allocat memory
+ *
+ * @b: int
+ *
+ * Return: pointr or NULL
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *m = malloc(b);
+
+	if (m == 0)
+		exit(98);
+
+	return (m);
+}
