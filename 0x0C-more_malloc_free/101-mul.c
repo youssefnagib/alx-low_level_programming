09abd8ac53e8ced641bb69ce5a39b6f9d6b3db30
youssefnagib <youssefnagib@yahoo.com>
2023-04-13 19:52:39 -0700

    yalla

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,14 +12,15 @@
 
 int is_digit(char *s)
 {
-	int i = 0;	
+	int i = 0;
+
 	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		i++;	
+		i++;
 	}
-	return (1);		
+	return (1);
 }
 
 /**
@@ -29,25 +30,24 @@ int is_digit(char *s)
  *
  * Return: len string
  */
-
-int _strlen(char *s)		
+int _strlen(char *s)
 {
 	int i = 0;
-	while (s[i] != '\0')	
-	{
-		i++;	
-	}
-	return (i);		
-}
 
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 /**
  * errors - handles errors
  */
 
-void errors(void)		
+void errors(void)
 {
 	printf("Error\n");
-	exit(98);		
+	exit(98);
 }
 
 /**
@@ -60,10 +60,11 @@ void errors(void)
  * Return: always 0
  */
 
-int main(int argc, char *argv[])		
+int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
@@ -75,7 +76,7 @@ int main(int argc, char *argv[])
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)	
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
@@ -87,18 +88,18 @@ int main(int argc, char *argv[])
 			carry /= 10;
 		}
 		if (carry > 0)
-			result[len1 + len2 + 1] += carry;	
+			result[len1 + len2 + 1] += carry;
 	}
-	for (i = 0; i < len - 1; i++)	
+	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
 			a = 1;
 		if (a)
-			_putchar(result[i] + '0');	
-	}	
+			_putchar(result[i] + '0');
+	}
 	if (!a)
 		_putchar('0');
 	_putchar('\n');
 	free(result);
-	return (0);		
+	return (0);
 }
