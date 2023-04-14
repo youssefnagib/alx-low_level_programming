5c6d9ceb153d0783cdd780d2423ec9d0f1cee9d8
youssefnagib <youssefnagib@yahoo.com>
2023-04-13 19:44:41 -0700

    yalla

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,135 +1,104 @@
 #include "main.h"
 #include <stdlib.h>
+#define ERR_MSG "Error"
 
 /**
- * _memset - fills memory
+ * is_digit - checks string
  *
- * @s: input pointer
+ * @s: string
  *
- * @b: set
- *
- * @n: num
- *
- * Return: pointer
-*/
+ * Return: 0 if a non-digit else 1
+ */
 
-char *_memset(char *s, char b, unsigned int n)
+int is_digit(char *s)
 {
-	unsigned int i = 0;
-
-	while (i < n)
+	int i = 0;	
+	while (s[i])
 	{
-		s[i] = b;
-		i++;
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;	
 	}
-	return (s);
+	return (1);		
 }
 
 /**
- * _calloc - allocates memory
+ * _strlen - returns the len of string
  *
- * @nmemb: size array
+ * @s: string
  *
- * @size: each element
- *
- * Return: pointer to new
-*/
+ * Return: len string
+ */
 
-void *_calloc(unsigned int nmemb, unsigned int size)
+int _strlen(char *s)		
 {
-	char *ptr;
-
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-		return (NULL);
-	_memset(ptr, 0, nmemb * size);
-
-	return (ptr);
-}
-
-
-/**
- * multiply - initialize array 0
- *
- * @s1: string 1
- *
- * @s2: string 2
- *
- * Return: 1, 98
-*/
-
-void multiply(char *s1, char *s2)
-{
-	int i, l1, l2, total_l, f_digit, s_digit, res = 0, tmp;
-	char *ptr;
-	void *temp;
-
-	l1 = _length(s1);
-	l2 = _length(s2);
-	tmp = l2;
-	total_l = l1 + l2;
-	ptr = _calloc(sizeof(int), total_l);
-
-	/* store our pointer to free later */
-	temp = ptr;
-
-	for (l1--; l1 >= 0; l1--)
+	int i = 0;
+	while (s[i] != '\0')	
 	{
-		f_digit = s1[l1] - '0';
-		res = 0;
-		l2 = tmp;
-		for (l2--; l2 >= 0; l2--)
+		i++;	
+	}
+	return (i);		
+}
+
+/**
+ * errors - handles errors
+ */
+
+void errors(void)		
+{
+	printf("Error\n");
+	exit(98);		
+}
+
+/**
+ * main - multiplies numbers
+ *
+ * @argc: num
+ *
+ * @argv: array
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])		
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)	
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			s_digit = s2[l2] - '0';
-			res += ptr[l2 + l1 + 1] + (f_digit * s_digit);
-			ptr[l1 + l2 + 1] = res % 10;
-			res /= 10;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
-		if (res)
-			ptr[l1 + l2 + 1] = res % 10;
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;	
 	}
-
-	while (*ptr == 0)
+	for (i = 0; i < len - 1; i++)	
 	{
-		ptr++;
-		total_l--;
-	}
-
-	for (i = 0; i < total_l; i++)
-		printf("%i", ptr[i]);
-	printf("\n");
-	free(temp);
-}
-
-
-/**
- * main - point
- *
- * Description: program multiplies
- *
- * @argc: num of arg
- *
- * @argv: arg array
- *
- * Return: 0 , 98
-*/
-
-int main(int argc, char *argv[])
-{
-	char *n1 = argv[1];
-	char *n2 = argv[2];
-
-	if (argc != 3 || check_number(n1) || check_number(n2))
-		error_exit();
-
-	if (*n1 == '0' || *n2 == '0')
-	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');	
+	}	
+	if (!a)
 		_putchar('0');
-		_putchar('\n');
-	}
-	else
-		multiply(n1, n2);
-	return (0);
+	_putchar('\n');
+	free(result);
+	return (0);		
 }
