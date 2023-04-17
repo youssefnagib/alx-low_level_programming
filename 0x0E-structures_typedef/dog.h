b201e8771ba00e6b31a6e5b2b0360bf90b58322e
youssefnagib <youssefnagib@yahoo.com>
2023-04-17 16:19:27 -0700

    yalla

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -25,5 +25,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
