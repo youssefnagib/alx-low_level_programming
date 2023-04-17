0b6fecbbf61bc932ac77caf8a08e3eacd0b97914
youssefnagib <youssefnagib@yahoo.com>
2023-04-17 14:43:09 -0700

    yalla

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - struct dog
+ *
+ * @name: name char
+ *
+ * @age: age float
+ *
+ * @owner: owner name char
+ *
+ * Description: name, age and owner of a dog
+ */
+
 int _putchar(char c);
 typedef struct dog
 {
