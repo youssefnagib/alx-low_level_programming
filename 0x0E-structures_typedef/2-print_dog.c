d33dd4d7de645887da98f46821e7d25031838acf
youssefnagib <youssefnagib@yahoo.com>
2023-04-17 15:17:15 -0700

    yalla

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,9 +1,11 @@
 #include "dog.h"
+
 /**
  * print_dog - print dog struct
  *
  * @d: dog struct
  */
+
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
@@ -11,7 +13,7 @@ void print_dog(struct dog *d)
 		printf("Name: %s\nAge: %f\nOwner: %s\n",
 				d->name == NULL ? "{nil)" : d->name,
 				d->age,
-				d->owner == NULL ? "{nil)" : d->owner,
+				d->owner == NULL ? "{nil)" : d->owner,);
 
 	}
 }
