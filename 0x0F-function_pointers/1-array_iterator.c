86eb124981f1c6a41bf60fdc41282d49655ea3e9
youssefnagib <youssefnagib@yahoo.com>
2023-04-19 19:33:29 -0700

    yalla

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers."
+/**
+ * array_iterator -  maps array through fun pointer
+ *
+ * @array: pointer to array
+ *
+ * @size: Size of the array
+ *
+ * @action: fun pointer
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int *end = array + size - 1;
+
+	if (array && size && action)
+	while (array <= end)
+		action(*array++);
+}
