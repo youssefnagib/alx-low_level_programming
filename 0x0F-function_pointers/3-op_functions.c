498fb5196b57de0f9652b310b77d7970c393e9be
youssefnagib <youssefnagib@yahoo.com>
2023-04-19 20:25:38 -0700

    yalla

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -8,9 +8,11 @@
 	int op_mod(int a, int b);
 
 /**
- * op_add - Returns the sum of two numbers.
- * @a: The first number.
- * @b: The second number.
+ * op_add - Returns the sum of two numbers
+ *
+ * @a: The first num.
+ *
+ * @b: The second num.
  *
  * Return: The sum of a and b.
  */
@@ -19,9 +21,12 @@ int op_add(int a, int b)
 	return (a + b);
 }
 /**
- * op_sub - Returns the difference of two numbers.
- * @a: The first number.
- * @b: The second number.
+ * op_sub - Returns the difference of two num.
+ *
+ * @a: The first num.
+ *
+ * @b: The second num.
+ *
  * Return: The difference of a and b.
   */
 int op_sub(int a, int b)
@@ -29,9 +34,11 @@ int op_sub(int a, int b)
 	return (a - b);
 }
 /**
- * op_mul - Returns the product of two numbers.
- * @a: The first number.
- * @b: The second number.
+ * op_mul - Returns the product of two num.
+ *
+ * @a: The first num.
+ *
+ * @b: The second num.
  *
  * Return: The product of a and b.
  */
@@ -40,9 +47,11 @@ int op_mul(int a, int b)
 	return (a * b);
 }
 /**
- * op_div - Returns the division of two numbers.
- * @a: The first number.
- * @b: The second number.
+ * op_div - Returns the division of two num.
+ *
+ * @a: The first num.
+ *
+ * @b: The second num.
  *
  * Return: The quotient of a and b.
  */
@@ -51,9 +60,11 @@ int op_div(int a, int b)
 	return (a / b);
 }
 /**
- * op_mod - Returns the remainder of the division of two numbers.
- * @a: The first number.
- * @b: The second number.
+ * op_mod - Returns the remainder of the division of two num.
+ *
+ * @a: The first num.
+ *
+ * @b: The second num.
  *
  * Return: The remainder of the division of a by b.
  */
