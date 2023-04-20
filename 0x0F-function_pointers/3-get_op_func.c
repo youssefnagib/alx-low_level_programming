bd036e62b776b550a50e79396ddf04b16eed098f
youssefnagib <youssefnagib@yahoo.com>
2023-04-19 20:35:53 -0700

    yalla

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,13 +24,9 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (i < 5)
-	{
-		if (strcmp(s, ops[i].op) == 0)
-		{
-			return (*(ops[i]).f);
-		}
-		i++;
-	}
-	return (NULL);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+			i++;
+
+
+		return (ops[i].f);
 }
