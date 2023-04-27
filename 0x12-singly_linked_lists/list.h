5be7ef705a4c5f61d667e793c063d3cef4bc2d3a
youssefnagib <youssefnagib@yahoo.com>
2023-04-28 00:19:48 +0200

    yalla

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-typedef strcut list_s
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
@@ -14,5 +14,10 @@ typedef strcut list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
 
 #endif // LIST_H
