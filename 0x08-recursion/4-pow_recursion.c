a467010d35ed0123774c5f1e6597b25feb0661d3
Youssefnagib <youssefnagib14@yahoo.com>
2023-06-08 04:11:48 +0200

    masr

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,24 @@
 #include "main.h"
-/***/
+/**
+ * _pow_recursion - power num and git retuen
+ *
+ * @x: input
+ *
+ * @y: input
+ *
+ * Return: (num)
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
+
 	else if (y == 0)
-		return(1);
+		return (1);
+
 	else if (y == 1)
 		return (x);
+
 	else
 	{
 		return (x * _pow_recursion(x, y - 1));
