88ab0298d96aec2830e3ff5a3cbf3662b12c051b
Youssefnagib <youssefnagib14@yahoo.com>
2023-06-08 03:42:53 +0200

    masr

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,11 +1,17 @@
 #include "main.h"
-/***/
+/**
+ * factorial - git factor of num
+ *
+ * @n: input
+ *
+ * Return: (factor)
+ */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
 		return (1);
-	else 
-		return n * factorial(n-1);
+	else
+		return (n * factorial(n - 1))
 }
