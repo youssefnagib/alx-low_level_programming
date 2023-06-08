7959e0d6b0cf6d03c356edf8e387a2d3e52c4fc2
Youssefnagib <youssefnagib14@yahoo.com>
2023-06-08 02:09:32 +0200

    masr

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - functin that print string
+ *
+ * @s: in put source
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+	else
+	_putchar('\n');
+}
