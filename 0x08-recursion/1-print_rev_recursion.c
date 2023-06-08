a00d287913ccc96eac95410d65c6c3f6c4b2cbe1
Youssefnagib <youssefnagib14@yahoo.com>
2023-06-08 02:27:48 +0200

    masr

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * */
+void _print_rev_recursion(char *s);
+{
+	int i = 0;
+	int t;
+
+	i++;
+	for (s[0] != '\0'; s++; i--)
+	{
+		i = t;
+	}
+	for (s[0] != '\0'; s--; t++)
+	{
+		s[0] == t;
+	}
+	return 1;
+}
