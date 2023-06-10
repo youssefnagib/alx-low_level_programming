f848670296f949228399018075a97cb02acd88b8
Youssefnagib <youssefnagib14@yahoo.com>
2023-06-10 04:54:46 +0200

    done

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,20 +9,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
-
-	for (i = 1; i < argc; i++)
+	int i;
+	
+	for (i = 0; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		add += atoi(argv[i]);
+		printf("%s\n", argv[i]);
 	}
-	printf("%d\n", add);
 	return (0);
 }
