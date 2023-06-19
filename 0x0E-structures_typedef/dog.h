f9c47a59efdacb9d474a9abff14e2073e4bf9ecb
Youssefnagib <youssefnagib14@yahoo.com>
2023-06-19 21:49:49 +0200

    0x0D

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - func that print name age owner
+ * @name: char name
+ * @age: fluat age
+ * @owner: char owner
+ * Description: Define struct dog
+ */
 
 struct dog
 {
