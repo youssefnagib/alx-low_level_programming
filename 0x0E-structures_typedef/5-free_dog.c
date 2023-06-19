e10fb3f8e2c2f4d21e17cbe587cbcaf482b41b3a
Youssefnagib <youssefnagib14@yahoo.com>
2023-06-19 22:52:30 +0200

    0x0D

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free struct
+ *
+ * @d: pointer to strct to freee
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
