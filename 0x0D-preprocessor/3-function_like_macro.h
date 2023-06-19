70f771ec33de10b4c7dbbb17d871bc4b2d9b8802
Youssefnagib <youssefnagib14@yahoo.com>
2023-06-19 21:14:41 +0200

    0x0D

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef FUNCTUON_LIKE_MACRO.H
-#define FUNCTUON_LIKE_MACRO.H
-#define ABS(x) x
+#ifndef _FUNCTUON_LIKE_MACRO.H
+#define -FUNCTUON_LIKE_MACRO.H
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 #endif
