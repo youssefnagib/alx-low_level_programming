9f8d11374dfe1be4d4fba4555b34c66237cfde7e
Youssefnagib <youssefnagib14@yahoo.com>
2023-06-19 22:24:00 +0200

    0x0D

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+/**
+ * _init_dog - func that init
+ *
+ * @d: Init
+ *
+ * @name: name
+ *
+ * @age: age
+ *
+ * @owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
