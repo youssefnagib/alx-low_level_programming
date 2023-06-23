9da00649b692039f82ec3faad9a129f826044f2f
Youssefnagib <youssefnagib14@yahoo.com>
2023-06-23 03:01:11 +0200

    0x0f

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+/**
+ * op_add - fun that add
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - func that sub
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - func that *
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - func that divedd
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - func that mod
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+		return (a % b);
+}
