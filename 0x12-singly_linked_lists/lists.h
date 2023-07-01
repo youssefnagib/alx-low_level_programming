8d1ce2c57c5626b5fa01ee244afe4f46c1255b18
youssefnagib <youssefnagib@yahoo.com>
2023-07-01 03:09:36 +0200

    yalla

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
