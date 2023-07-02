1ab69b4306816ef25acc6b8fe90ae9c0df2083de
youssefnagib <youssefnagib@yahoo.com>
2023-07-02 15:24:23 +0200

    yalla

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/*
+ * add_node - add node
+ * @head: Pointer list
+ * @str: String
+ * Return: address
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int len;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	len = 0;
+	while (str[len])
+	len++;
+	new->next = *head;
+	new->len = len;
+	*head = new;
+
+	return (new);
+}
