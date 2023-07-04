7028448159885999514b4c205732c1b1face544b
youssefnagib <youssefnagib@yahoo.com>
2023-07-04 15:02:49 +0200

    yalla

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,9 +12,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		printf("%d", h->n);
-		counter++;
+		printf("%d\n", h->n);
 		h = h->next;
+		counter++;
 	}
-return (counter)
+	return (counter);
 }
