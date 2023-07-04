5e37bfdc4714bc8dcf7507b7aebea7d152e71f8e
youssefnagib <youssefnagib@yahoo.com>
2023-07-04 15:05:56 +0200

    yalla

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,11 +6,12 @@
 #include <stddef.h>
 
 
-struct *Node {
-	int data;
-	struct Node* next;
-	struct NOde* prev;
-};
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
