da12ea0ef8633a865ff632518b84d14af237cfee
youssefnagib <youssefnagib@yahoo.com>
2023-07-05 17:47:49 +0200

    yalla

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 /**
+ * add_nodeint - function that add node
  *
+ * @head: head of the nodes
+ *
+ * @n: data int
+ *
+ * Return: address or NULL
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
