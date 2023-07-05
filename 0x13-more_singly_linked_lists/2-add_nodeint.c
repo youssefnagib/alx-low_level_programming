5525159c6341c072286d089cc4f58068d4ed4e9f
youssefnagib <youssefnagib@yahoo.com>
2023-07-05 17:33:50 +0200

    yalla

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,7 +9,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (n != newNode)
 		return (NULL);
 	newNode->n = n;
-	newNode->next = (*head);
+	newNode->next = *head;
 	*head = newNode;
 		return (newNode);
 }
