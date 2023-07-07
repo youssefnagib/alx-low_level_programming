999711bfc3fd06af25a83ba07d987756905fe96b
youssefnagib <youssefnagib@yahoo.com>
2023-07-07 23:05:17 +0200

    0x14

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * binary_to_uint - func that convert bin to dec
+ *
+ * @b: char input
+ *
+ * Return: reslute if not NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int r = 0;
+	int i;
+
+	if (!b)
+		return (0);
+
+	while (b[i] != '\0')
+	{
+		r <<= 1;
+		if (b[i] == '1')
+		{
+			r += 1;
+		}
+			i++;
+	}
+	return (r);
+}
