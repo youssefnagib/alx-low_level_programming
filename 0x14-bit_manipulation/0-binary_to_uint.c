c2b75dd33da19b6a53cb1a1622d781cc1d2aa0a6
youssefnagib <youssefnagib@yahoo.com>
2023-07-07 23:16:39 +0200

    0x14

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,7 +10,7 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int digit = 0;
+	unsigned int r = 0;
 	unsigned int i = 0;
 
 	if (!b)
@@ -28,12 +28,12 @@ unsigned int binary_to_uint(const char *b)
 	i = 0;
 	while (b[i] != '\0')
 	{
-		digit <<= 1;
+		r <<= 1;
 		if (b[i] == '1')
 		{
-			digit += 1;
+			r += 1;
 		}
 			i++;
 	}
-	return (digit);
+	return (r);
 }
