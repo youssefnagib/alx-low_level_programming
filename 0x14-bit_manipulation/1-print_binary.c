5d463772fed1e9458cecd13bb34080582fc2cfcd
youssefnagib <youssefnagib@yahoo.com>
2023-07-08 00:27:45 +0200

    0x14

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,21 +8,22 @@
 
 void print_binary(unsigned long int n)
 {
-	int i, count = 0;
-	unsigned long int current;
+	int d, binary, flag;
 
-	for (i = 63; i >= 0; i--)
-	{
-		current = n >> i;
-
-		if (current & 1)
-		{
-			_putchar('1');
-			count++;
-		}
-		else if (count)
-			_putchar('0');
-	}
-	if (!count)
+	flag = 0;
+	if (n == 0)
 		_putchar('0');
+	for  (d = 63; d >= 0; d--)
+	{
+		binary = (n >> d);
+		if (binary != 0)
+			flag = 1;
+		if (flag != 0)
+		{
+			if (binary & 1)
+				_putchar('1');
+			else
+				_putchar('0');
+		}
+	}
 }
