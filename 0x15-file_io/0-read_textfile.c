6f4397b325cabfb88e32987fb1f8e17942cbe01b
youssefnagib <youssefnagib@yahoo.com>
2023-07-11 02:44:25 +0200

    15

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main"
+/**
+ * read_textfile - read text
+ *
+ * @filename: name
+ *
+ * @letters: letters
+ * Return : 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t fd, my_read, my_write;
+
+	if (!filename)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	my_read = read(fd, buffer, letters);
+	my_write = write(STDOUT_FILENO, buffer, my_read);
+	if (fd == -1 || my_read == -1 || my_write != my_read)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+
+	return (my_write);
+}
