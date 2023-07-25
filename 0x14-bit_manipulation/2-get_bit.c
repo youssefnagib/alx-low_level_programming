da4b9fbb0f5fec147e09f6204c8c979eb9e91113
youssefnagib <youssefnagib@yahoo.com>
2023-07-25 19:14:10 +0200

    14

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * get_bit - func returns the value of a bit at a given index
+ * @n: input
+ * @index: input
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+	n >>= index;
+	if (n & 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
